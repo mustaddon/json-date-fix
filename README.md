a109c89d1a52b939eec5c907e48e8c3f56fd8eb9
Leonid Salavatov <mustaddon@gmail.com>
2022-08-28 19:32:22 +0300

    v0.1.0

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# json-date-fix
+# json-date-fix [![npm version](https://badge.fury.io/js/json-date-fix.svg)](https://www.npmjs.com/package/json-date-fix)
+Memory usage optimization in cascading iterable queries
+
+```js
+import 'json-date-fix' // need once at the root of your app
+
+// serialization fix: preserve timezone information
+console.log(JSON.stringify({date:new Date()}));
+
+// deserialization fix: replace date-string to date-object
+console.log(JSON.parse('{"date":"2022-08-28T19:19:55.813+03:00"}'));
+
+
+//// Console output:
+// {"date":"2022-08-28T19:19:55.813+03:00"}
+// {date: Sun Aug 28 2022 19:19:55 GMT+0300 (Moscow Standard Time)}
+```
---
+++ index.js
@@ -0,0 +1,37 @@
+// deserialization fix: replace date-string to date-object
+const isoDatePattern = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}[.Z:+\-\d]*?$/;
+const dotnetDatePattern = /^\/Date\((\d+)([+\-\d]*)\)\/$/;
+const jsonParseOrigin = JSON.parse;
+const jsonParseFixed = (text, reviver) => jsonParseOrigin(text, (k, v) => {
+    if (reviver)
+        v = reviver(k, v);
+
+    if (isoDatePattern.test(v))
+        return new Date(v);
+
+    let tmp; if (tmp = dotnetDatePattern.exec(v))
+        return new Date(tmp.slice(1, 3).reduce((a, x) => a + (parseInt(x) || 0), 0));
+
+    return v;
+});
+
+if (JSON.parse != jsonParseFixed)
+    JSON.parse = jsonParseFixed;
+
+
+// serialization fix: preserve timezone information
+const ddFormat = (x) => (100 + Math.floor(Math.abs(x))).toString().slice(-2);
+const dateToIsoOrigin = Date.prototype.toISOString;
+const dateToJsonFixed = function () {
+    const offset = this.getTimezoneOffset();
+    return [
+        dateToIsoOrigin.call(new Date(this.getTime() - offset * 60000)).slice(0, -1),
+        offset > 0 ? '-' : '+',
+        ddFormat(offset / 60),
+        ':',
+        ddFormat(offset % 60),
+    ].join('');
+};
+
+if (Date.prototype.toJSON != dateToJsonFixed && dateToIsoOrigin.call(new Date).slice(-1) == 'Z')
+    Date.prototype.toJSON = dateToJsonFixed;
