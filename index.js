223962dce4d3ee1d7f48600d9f4db48151e1cad2
Leonid Salavatov <mustaddon@gmail.com>
2022-08-28 20:58:54 +0300

    v1.0.1

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 const isoDatePattern = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}[.Z:+\-\d]*?$/;
 const dotnetDatePattern = /^\/Date\((\d+)([+\-\d]*)\)\/$/;
 const jsonParseOrigin = JSON.parse;
-const jsonParseFixed = (text, reviver) => jsonParseOrigin(text, (k, v) => {
+
+JSON.parse = (text, reviver) => jsonParseOrigin(text, (k, v) => {
     if (reviver)
         v = reviver(k, v);
 
@@ -15,23 +16,19 @@ const jsonParseFixed = (text, reviver) => jsonParseOrigin(text, (k, v) => {
     return v;
 });
 
-if (JSON.parse != jsonParseFixed)
-    JSON.parse = jsonParseFixed;
-
 
 // serialization fix: preserve timezone information
 const ddFormat = (x) => (100 + Math.floor(Math.abs(x))).toString().slice(-2);
 const dateToIsoOrigin = Date.prototype.toISOString;
-const dateToJsonFixed = function () {
-    const offset = this.getTimezoneOffset();
-    return [
-        dateToIsoOrigin.call(new Date(this.getTime() - offset * 60000)).slice(0, -1),
-        offset > 0 ? '-' : '+',
-        ddFormat(offset / 60),
-        ':',
-        ddFormat(offset % 60),
-    ].join('');
-};
 
-if (Date.prototype.toJSON != dateToJsonFixed && dateToIsoOrigin.call(new Date).slice(-1) == 'Z')
-    Date.prototype.toJSON = dateToJsonFixed;
+if (dateToIsoOrigin.call(new Date).slice(-1) == 'Z')
+    Date.prototype.toJSON = function () {
+        const offset = this.getTimezoneOffset();
+        return [
+            dateToIsoOrigin.call(new Date(this.getTime() - offset * 60000)).slice(0, -1),
+            offset > 0 ? '-' : '+',
+            ddFormat(offset / 60),
+            ':',
+            ddFormat(offset % 60),
+        ].join('');
+    };
