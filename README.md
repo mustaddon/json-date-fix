bae9d066f1cbf1eb41c79b19922f22c0db024a0e
Leonid Salavatov <mustaddon@gmail.com>
2024-09-19 22:59:30 +0300

    dateonly

--- README.md
+++ README.md
@@ -1,17 +1,33 @@
 # json-date-fix [![npm version](https://badge.fury.io/js/json-date-fix.svg)](https://www.npmjs.com/package/json-date-fix)
 JSON serialization/deserialization fix for Date objects
 
+
 ```js
 import 'json-date-fix' // need once at the root of your app
 
-// serialization fix: preserve timezone information
-console.log(JSON.stringify({ date: new Date(2023, 5, 7) }));
 
-// deserialization fix: change date-string to date-object
-console.log(JSON.parse('{"date":"2023-06-07T00:00:00.000+03:00"}'));
+// serialization fix: preserve timezone information
+console.log(JSON.stringify(new Date(2023, 5, 7)));
+
+// deserialization date-string to Date object
+console.log(JSON.parse('"2023-06-07T00:00:00.000+03:00"'));
 
 
 //// Console output:
-// {"date":"2023-06-07T00:00:00.000+03:00"}
-// {date: Wed Jun 07 2023 00:00:00 GMT+0300 (Moscow Standard Time)}
+// "2023-06-07T00:00:00.000+03:00"
+// Wed Jun 07 2023 00:00:00 GMT+0300 (Moscow Standard Time)
+```
+
+
+Example with DateOnly format support
+```js
+import jsonDateFix from './index.js'; 
+jsonDateFix({ dateonly: true });  // enable dateonly format ('yyyy-MM-dd') analysis 
+
+
+// deserialization dateonly-string to Date object
+console.log(JSON.parse('"2023-06-07"'));
+
+//// Console output:
+// Wed Jun 07 2023 00:00:00 GMT+0300 (Moscow Standard Time)
 ```
--- index.js
+++ index.js
@@ -1,39 +1,51 @@
-// serialization fix: preserve timezone information
 const dateToIsoOrigin = Date.prototype.toISOString;
 const ddFormat = (x) => (100 + Math.floor(Math.abs(x))).toString().slice(-2);
-
-if (dateToIsoOrigin.call(new Date).slice(-1) == 'Z')
-    Date.prototype.toJSON = function () {
-        const offset = this.getTimezoneOffset();
-        return [
-            dateToIsoOrigin.call(new Date(this.getTime() - offset * 60000)).slice(0, -1),
-            offset > 0 ? '-' : '+',
-            ddFormat(offset / 60),
-            ':',
-            ddFormat(offset % 60),
-        ].join('');
-    };
-
-
-// deserialization fix: change date-string to date-object
 const jsonParseOrigin = JSON.parse;
 const isoDatePattern = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}[.Z:+\-\d]*?$/;
+const dateonlyPattern = /^\d{4}-\d{2}-\d{2}$/;
 const dotnetDatePattern = /^\/Date\((\d+)([+\-\d]*)\)\/$/;
 
-JSON.parse = (text, reviver) => jsonParseOrigin(text, (k, v) => {
-    if (reviver)
-        v = reviver(k, v);
 
-    if (isoDatePattern.test(v))
-        return new Date(v);
 
-    let tmp; if (tmp = dotnetDatePattern.exec(v))
-        return new Date(tmp.slice(1, 3).reduce((a, x) => a + (parseInt(x) || 0), 0));
+/** @param {{dotnet?: boolean, dateonly?: boolean}} [options] */
+export default function apply(options){
+    options = { ...options };
+    
+    // serialization fix: preserve timezone information
+    if (dateToIsoOrigin.call(new Date).slice(-1) == 'Z')
+        Date.prototype.toJSON = function () {
+            const offset = this.getTimezoneOffset();
+            return [
+                dateToIsoOrigin.call(new Date(this.getTime() - offset * 60000)).slice(0, -1),
+                offset > 0 ? '-' : '+',
+                ddFormat(offset / 60),
+                ':',
+                ddFormat(offset % 60),
+            ].join('');
+        };
+    
+    // deserialization fix: change date-string to date-object
+    JSON.parse = (text, reviver) => jsonParseOrigin(text, (k, v) => {
+        if (reviver)
+            v = reviver(k, v);
+    
+        if (isoDatePattern.test(v))
+            return new Date(v);
 
-    return v;
-});
+        if(options.dateonly && dateonlyPattern.test(v))
+            return new Date(v + 'T00:00:00');
+        
+        let tmp; 
+        if (options.dotnet && (tmp = dotnetDatePattern.exec(v)))
+            return new Date(tmp.slice(1, 3).reduce((a, x) => a + (parseInt(x) || 0), 0));
+    
+        return v;
+    });
 
-if (window.Response)
-    Response.prototype.json = async function () {
-        return JSON.parse(await this.text());
-    }
+    if (window.Response)
+        Response.prototype.json = async function () {
+            return JSON.parse(await this.text());
+        }
+};
+
+apply();
